a0ff622e3e9489de858a1257dfb268a8aaf2415f
davr59 <davr59@outlook.com>
2019-12-29 20:01:32 -0600

    update packages

--- firestore2json.js
+++ firestore2json.js
@@ -1,3 +1,7 @@
+/* eslint-disable function-paren-newline */
+/* eslint-disable implicit-arrow-linebreak */
+/* eslint-disable arrow-parens */
+/* eslint-disable comma-dangle */
 /* eslint-disable no-console */
 
 const admin = require('firebase-admin');
@@ -9,29 +13,33 @@ function firestore2json(serviceAccount, schema, outputFilePath) {
   const f2j = async (db, _schema, _current) => {
     const current = _current;
     await Promise.all(
-      Object.keys(_schema).map(collection => db
-        .collection(collection)
-        .get()
-        .then((data) => {
-          const promises = [];
-          data.forEach((doc) => {
-            if (!current[collection]) current[collection] = { __type__: 'collection' };
-            current[collection][doc.id] = doc.data();
-            promises.push(
-              f2j(
-                db.collection(collection).doc(doc.id),
-                _schema[collection],
-                current[collection][doc.id],
-              ),
-            );
-          });
-          return Promise.all(promises);
-        })),
+      Object.keys(_schema).map(collection =>
+        db
+          .collection(collection)
+          .get()
+          .then(data => {
+            const promises = [];
+            data.forEach(doc => {
+              if (!current[collection]) {
+                current[collection] = { __type__: 'collection' };
+              }
+              current[collection][doc.id] = doc.data();
+              promises.push(
+                f2j(
+                  db.collection(collection).doc(doc.id),
+                  _schema[collection],
+                  current[collection][doc.id]
+                )
+              );
+            });
+            return Promise.all(promises);
+          })
+      )
     );
     return current;
   };
 
-  f2j(admin.firestore(), { ...schema }, {}).then((res) => {
+  f2j(admin.firestore(), { ...schema }, {}).then(res => {
     fs.writeFileSync(outputFilePath, JSON.stringify(res, null, 2), 'utf8');
     console.log('done');
   });
--- json2firestore.js
+++ json2firestore.js
@@ -1,3 +1,7 @@
+/* eslint-disable function-paren-newline */
+/* eslint-disable implicit-arrow-linebreak */
+/* eslint-disable arrow-parens */
+/* eslint-disable comma-dangle */
 /* eslint-disable no-console */
 
 const admin = require('firebase-admin');
@@ -6,35 +10,38 @@ const fs = require('fs');
 function json2firestore(serviceAccount, schema, inputFilePath) {
   admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
 
-  const j2f = (json, db, _schema) => Promise.all(
-    Object.keys(_schema).map((collection) => {
-      const promises = [];
-      Object.keys(json[collection]).forEach((doc) => {
-        const docId = doc;
-        if (doc === '__type__') return;
-        const docData = Object.assign({}, json[collection][doc]);
-        Object.keys(docData).forEach((data) => {
-          // eslint-disable-next-line no-underscore-dangle
-          if (docData[data] && docData[data].__type__) delete docData[data];
+  const j2f = (json, db, _schema) =>
+    Promise.all(
+      Object.keys(_schema).map(collection => {
+        const promises = [];
+        Object.keys(json[collection]).forEach(doc => {
+          const docId = doc;
+          if (doc === '__type__') return;
+          const docData = { ...json[collection][doc] };
+          Object.keys(docData).forEach(data => {
+            // eslint-disable-next-line no-underscore-dangle
+            if (docData[data] && docData[data].__type__) delete docData[data];
+          });
+          promises.push(
+            db
+              .collection(collection)
+              .doc(docId)
+              .set(docData)
+              .then(() =>
+                j2f(
+                  json[collection][doc],
+                  db.collection(collection).doc(docId),
+                  _schema[collection]
+                )
+              )
+          );
         });
-        promises.push(
-          db
-            .collection(collection)
-            .doc(docId)
-            .set(docData)
-            .then(() => j2f(
-              json[collection][doc],
-              db.collection(collection).doc(docId),
-              _schema[collection],
-            )),
-        );
-      });
-      return Promise.all(promises);
-    }),
-  );
+        return Promise.all(promises);
+      })
+    );
 
   j2f(JSON.parse(fs.readFileSync(inputFilePath, 'utf8')), admin.firestore(), {
-    ...schema,
+    ...schema
   }).then(() => console.log('done'));
 }
 
