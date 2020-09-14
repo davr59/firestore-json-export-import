2b552e9c70c57c28048037c81bf248d19c5653a5
David V <[davr59@outlook.com](mailto:davr59@outlook.com)>
2020-09-13 22:08:08 -0600

    export subcollections

--- firestore2json.js
+++ firestore2json.js
@@ -1,49 +1,51 @@
 const admin = require('firebase-admin');
 const fs = require('fs');
 
-function firestore2json(serviceAccount, schema, outputFilePath) {
+async function firestore2json(serviceAccount, schema, outputFilePath) {
   admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
 
-  const f2j = async (db, _schema, _current) => {
-    const current = _current;
-    await Promise.all(
-      Object.keys(_schema).map((collection) =>
-        db
-          .collection(collection)
-          .get()
-          .then((data) => {
-            const promises = [];
-            data.forEach((doc) => {
-              if (!current[collection]) {
-                current[collection] = { __type__: 'collection' };
-              }
-              const docData = doc.data();
-              for (let key of Object.keys(docData)) {
-                if (!docData[key]._path) {
-                  continue;
-                }
-                docData[key] = { _path: docData[key].path };
-              }
-              current[collection][doc.id] = docData;
-              promises.push(
-                f2j(
-                  db.collection(collection).doc(doc.id),
-                  _schema[collection],
-                  current[collection][doc.id]
-                )
-              );
-            });
-            return Promise.all(promises);
-          })
-      )
-    );
-    return current;
+  const f2j_subcollection = async (doc, collection_id, _data) => {
+    let data = _data;
+    const subcollections = await doc.ref.listCollections();
+    for (let subcollection of subcollections) {
+      const dbSubcollection = await subcollection.get();
+      data[collection_id][doc.id][subcollection.id] = {};
+      await f2j_collection(dbSubcollection, subcollection.id, data[collection_id][doc.id]);
+    }
   };
 
-  f2j(admin.firestore(), { ...schema }, {}).then((res) => {
-    fs.writeFileSync(outputFilePath, JSON.stringify(res, null, 2), 'utf8');
-    console.log('done');
-  });
+  const f2j_collection = async (dbCollection, collection_id, _data) => {
+    let data = _data;
+    const promises = [];
+    dbCollection.forEach((doc) => {
+      const docData = doc.data();
+      Object.keys(docData).forEach((key) => {
+        if (!docData[key]._path) {
+          return;
+        }
+        docData[key] = { _path: docData[key].path };
+      });
+      data[collection_id][doc.id] = docData;
+      promises.push(f2j_subcollection(doc, collection_id, data));
+    });
+    await Promise.all(promises);
+  };
+
+  const f2j = async (db, _schema) => {
+    const data = {};
+    for (let collection_id of Object.keys(_schema)) {
+      if (!data[collection_id]) {
+        data[collection_id] = { __type__: 'collection' };
+      }
+      const dbCollection = await db.collection(collection_id).get();
+      await f2j_collection(dbCollection, collection_id, data);
+    }
+    return data;
+  };
+
+  const json = await f2j(admin.firestore(), { ...schema });
+  fs.writeFileSync(outputFilePath, JSON.stringify(json, null, 2), 'utf8');
+  console.log('done');
 }
 
 exports.firestore2json = firestore2json;
