866d70d33cb6b6621eef6e94eaa671403cda4a95
David V <[davr59@outlook.com](mailto:davr59@outlook.com)>
2020-09-20 15:23:56 -0600

    write subcollections

--- json2firestore.js
+++ json2firestore.js
@@ -1,47 +1,34 @@
 const admin = require('firebase-admin');
 const fs = require('fs');
 
-function json2firestore(serviceAccount, schema, inputFilePath) {
+async function json2firestore(serviceAccount, schema, inputFilePath) {
   admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
 
-  const j2f = (json, db, _schema) =>
-    Promise.all(
-      Object.keys(_schema).map((collection) => {
-        const promises = [];
-        if (!json[collection]) {
-          return;
-        }
-        Object.keys(json[collection]).forEach((doc) => {
-          const docId = doc;
-          if (doc === '__type__') return;
-          const docData = { ...json[collection][doc] };
-          Object.keys(docData).forEach((data) => {
-            if (docData[data] && docData[data].__type__) delete docData[data];
-            if (docData[data]._path) {
-              docData[data] = db.doc(docData[data]._path);
-            }
-          });
-          promises.push(
-            db
-              .collection(collection)
-              .doc(docId)
-              .set(docData)
-              .then(() =>
-                j2f(
-                  json[collection][doc],
-                  db.collection(collection).doc(docId),
-                  _schema[collection]
-                )
-              )
-          );
+  const j2f = async (json, db, _schema) => {
+    for (let collection of Object.keys(_schema)) {
+      if (!json[collection]) {
+        continue;
+      }
+      for (let doc of Object.keys(json[collection])) {
+        const docId = doc;
+        if (doc === '__type__') continue;
+        const docData = { ...json[collection][doc] };
+        Object.keys(docData).forEach((data) => {
+          if (docData[data] && docData[data].__type__) delete docData[data];
+          if (docData[data] && docData[data]._path) {
+            docData[data] = db.doc(docData[data]._path);
+          }
         });
-        return Promise.all(promises);
-      })
-    );
+        await db.collection(collection).doc(docId).set(docData);
+        await j2f(json[collection][doc], db.collection(collection).doc(docId), _schema[collection]);
+      }
+    }
+  };
 
-  j2f(JSON.parse(fs.readFileSync(inputFilePath, 'utf8')), admin.firestore(), {
+  await j2f(JSON.parse(fs.readFileSync(inputFilePath, 'utf8')), admin.firestore(), {
     ...schema,
-  }).then(() => console.log('done'));
+  });
+  console.log('done');
 }
 
 exports.json2firestore = json2firestore;
